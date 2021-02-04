9a0770f7024989ded101fa5f40b772ee85e1a525
yoshi-automation <yoshi-automation@google.com>
2021-02-04 02:25:45 -0800

    changes without context
            autosynth cannot find the source of changes triggered by earlier changes in this
            repository, or by version upgrades to tools such as linters.

--- system-test/fixtures/sample/src/index.js
+++ system-test/fixtures/sample/src/index.js
@@ -17,7 +17,7 @@
 // ** All changes to this file may be overwritten. **
 
 /* eslint-disable node/no-missing-require, no-unused-vars */
-const retail = require('@google-cloud/retail');
+const retail = require('retail');
 
 function main() {
   const catalogServiceClient = new retail.CatalogServiceClient();
